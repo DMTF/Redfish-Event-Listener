9738ef3e34d8c36b7431df41ff34a9b2aa310cc5
Mike Raineri <michael.raineri@dell.com>
2018-03-23 10:27:53 -0400

    Corrected link to license in source files

--- RedfishEventListener_v1.py
+++ RedfishEventListener_v1.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 # Copyright Notice:
 # Copyright 2017 Distributed Management Task Force, Inc. All rights reserved.
-# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Event-Listener/LICENSE.md
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Event-Listener/blob/master/LICENSE.md
 
 import socket
 import time
